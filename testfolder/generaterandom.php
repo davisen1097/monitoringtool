d268e40aebf46c5456e21f6d6b68b4e249543aea
davisen1097 <davisenmooken10@gmail.com>
2022-09-11 19:45:33 +0400

    generaterandomupdated

--- testfolder/generaterandom.php
+++ testfolder/generaterandom.php
@@ -1,9 +1,14 @@
 <?php
+ 
+ 
+ function generateRandomString($length = 8) {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
-
-$n = 4;
-$result = bin2hex(random_bytes($n));
-echo $result;
-
-?>
-
+echo generateRandomString();
