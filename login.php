cf5aae945937266c4a946256f1983e513f28fdbb
Laurent Petit <31487443+LaureLazard@users.noreply.github.com>
2022-09-11 19:51:15 +0400

    changed login logic

--- login.php
+++ login.php
@@ -1,48 +1,47 @@
 <?php
+    session_start();
 
-session_start();
+    include("connection.php");
+    include("functions.php");
+    require("twoFA.php");
+    $message = "PHP read Error";
 
-include("connection.php");
-include("functions.php");
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $message = "DB connection error";
 
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    //something was posted
-    $user_name = $_POST['user_name'];
-    $password = md5($_POST['password']);
-    $myObj =  new \stdClass();
-    $message = "";
+        $user_name = $_POST['user_name'];
+        $password = md5($_POST['password']);
+        $myObj =  new \stdClass();
 
-    if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+        if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+            $message = "SQL query error";
 
-        //read from database
-        $query = "select * from users where user_name = '$user_name' limit 1";
-        $result = mysqli_query($con, $query);
+            $query = "select * from users where user_name = '$user_name' limit 1";
+            $result = mysqli_query($con, $query);
 
-        if ($result) {
-            if ($result && mysqli_num_rows($result) > 0) {
+            if($result){
+                $message = "Query Result Error";
 
-                $user_data = mysqli_fetch_assoc($result);
+                if ($result && mysqli_num_rows($result) > 0) {
+                    $user_data = mysqli_fetch_assoc($result);
+                    if ($user_data['password'] === $password) {
 
-                if ($user_data['password'] === $password) {
+                        generate2FA($user_data['id']);
+                        $message="user authenticated";
+                        // header("Location: index.html");
+                        // die;
+                    }
 
-                    $_SESSION['user_id'] = $user_data['id'];
-                    // header("Location: index.html");
-                    // die;
-                }
+                } else{ $message="wrong username or password!";}
             }
-        }
-        else{ $message="wrong username or password!";}
-    } else {
-        $message="invalid username or password!";
-    }
-    
-    $myObj->message = $message;
 
+        } else { $message="invalid username or password!"; }
+            
+        $myObj->authmessage = $message;
+        $myJSON = json_encode($myObj);
+        echo $myJSON;
+    } 
 
-    $myJSON = json_encode($myObj);
-    
-    echo $myJSON;
-}
 
 ?>
